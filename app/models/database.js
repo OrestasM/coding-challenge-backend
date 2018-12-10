a83aa15b382e50c445f5d28fa67284d3e8cc1842
OrestasM <orestas.mazeikaa@gmail.com>
2018-12-10 08:01:55 +0200

    Fixed database connection

--- app/models/database.js
+++ app/models/database.js
@@ -1,20 +1,21 @@
 const mysql = require('mysql');
 let config = require('config');
 
-// const db = mysql.createConnection({
-//     host: 'todolist.cv7zpivicwkt.eu-central-1.rds.amazonaws.com',
-//     user: 'root',
-//     password: 'rootroot',
-//     port: '3306'
-// });
-
-
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'todolist.cv7zpivicwkt.eu-central-1.rds.amazonaws.com',
     user: 'root',
-    password: 'root',
+    password: 'rootroot',
     port: '3306'
 });
+
+// Uncomment when testing
+// Got to use localhost MySQL database
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     port: '3306'
+// });
 
 console.log(config.util.getEnv('NODE_ENV'))
 
